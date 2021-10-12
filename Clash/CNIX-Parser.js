36609c2c54082ffce18679a994464163fd738af7
赵旭阳 <zhaoxuyang@wondersgroup.com>
2021-10-12 23:59:17 +0800

    添加沪港负载

--- Clash/CNIX-Parser.js
+++ Clash/CNIX-Parser.js
@@ -1,44 +1,46 @@
 module.exports.parse = async (raw, {axios, yaml, notify, console}, {name, url, interval, selected}) => {
 
+    function extracted(proxiesGroupName, searchElement) {
+        let proxies = [];
+        for (let proxy of content.proxies) {
+            if (proxy.server === undefined) continue;
+            if (proxy.name.indexOf(searchElement) !== -1) {
+                proxies.push(proxy.name);
+            }
+        }
+        if (proxies.length > 0) {
+            content['proxy-groups'].push({
+                'name': proxiesGroupName,
+                'type': 'load-balance',
+                'proxies': proxies,
+                'url': 'http://cp.cloudflare.com/generate_204',
+                'interval': 300,
+                'strategy': 'consistent-hashing'
+            });
+            content['proxy-groups'][0].proxies.unshift(proxiesGroupName);
+        }
+    }
     console.log('yaml---')
     console.log(yaml)
 
     notify("MiXin 解析：", url, false);
     const content = yaml.parse(raw)
-    // console.log(obj)
-
-    let proxies = [];
-    for (let proxy of content.proxies) {
-        if (proxy.server === undefined) continue;
-        if (proxy.name.indexOf('香港') !== -1) {
-            proxies.push(proxy.name);
-        }
-    }
-    if (proxies.length > 0) {
-        content['proxy-groups'].push({
-            'name': '🇭🇰香港负载均衡',
-            'type': 'load-balance',
-            'proxies': proxies,
-            'url': 'http://cp.cloudflare.com/generate_204',
-            'interval': 300,
-            'strategy': 'consistent-hashing'
-        });
-        content['proxy-groups'][0].proxies.unshift("🇭🇰香港负载均衡");
-    }
+    extracted('🇭🇰香港负载均衡','香港');
+    extracted('🇭🇰沪港负载','沪港');
 
     // 导入神机规则
 
     // 导入 Global 全球常用域名的规则
     let ruleProviders = {}
     ruleProviders['Global'] = {
-            'type': "http",
-            'behavior': "classical",
-            'path': "./Global.yaml",
-            'url': 'https://raw.githubusercontent.com/DivineEngine/Profiles/master/Clash/RuleSet/Global.yaml',
-            'interval': "86400"
+        'type': "http",
+        'behavior': "classical",
+        'path': "./Global.yaml",
+        'url': 'https://raw.githubusercontent.com/DivineEngine/Profiles/master/Clash/RuleSet/Global.yaml',
+        'interval': "86400"
     }
     content['rule-providers'] = ruleProviders
-        // 将上述的 Global 设置为使用代理
+    // 将上述的 Global 设置为使用代理
     content['rules'].unshift("RULE-SET,Global,🔰国外流量");
 
     // // 导入 Unbreak 列表
