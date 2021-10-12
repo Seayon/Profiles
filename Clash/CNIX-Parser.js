755288cbdb04112e54a1ab6753b1a8d1bee710ea
赵旭阳 <zhaoxuyang@wondersgroup.com>
2021-10-12 23:25:15 +0800

    完善

--- Clash/CNIX-Parser.js
+++ Clash/CNIX-Parser.js
@@ -7,7 +7,7 @@ module.exports.parse = async (raw, {axios, yaml, notify, console}, {name, url, i
     const content = yaml.parse(raw)
     // console.log(obj)
 
-    proxies = [];
+    let proxies = [];
     for (let proxy of content.proxies) {
         if (proxy.server === undefined) continue;
         if (proxy.name.indexOf('香港') !== -1) {
@@ -25,6 +25,31 @@ module.exports.parse = async (raw, {axios, yaml, notify, console}, {name, url, i
         });
         content['proxy-groups'][0].proxies.unshift("🇭🇰香港负载均衡");
     }
+
+    // 导入神机规则
+
+    // 导入 Global 全球常用域名的规则
+    let ruleProviders = {}
+    content['rule-providers'] = ruleProviders['Global'] = {
+        'type': "http",
+        'behavior': "classical",
+        'path': "./Global.yaml",
+        'url': 'https://raw.githubusercontent.com/DivineEngine/Profiles/master/Clash/RuleSet/Global.yaml',
+        'interval': "86400",
+    }
+    // 将上述的 Global 设置为使用代理
+    content['rules'].unshift("RULE-SET,Global,🔰国外流量");
+
+    // // 导入 Unbreak 列表
+    // content['rules'].unshift("RULE-SET,Global,🔰国外流量");
+
+
+    // 导入医保内网要放开的网段
+    content['rules'].unshift("IP-CIDR,172.15.0.0/16,DIRECT");
+    content['rules'].unshift("IP-CIDR,168.151.0.0/16,DIRECT");
+    content['rules'].unshift("IP-CIDR,168.10.0.0/16,DIRECT");
+    content['rules'].unshift("IP-CIDR,168.20.0.0/16,DIRECT");
+    content['rules'].unshift("IP-CIDR,168.100.0.0/16,DIRECT");
 
     return yaml.stringify(content)
 }
